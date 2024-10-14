ceb492556fdf9bfa0e1b9ded7e71b87c49a8078d
Ved Thiru <47309279+PerpetualCreativity@users.noreply.github.com>
2024-10-14 09:10:47 -0400

    working wifi on foundry. bluetooth untested

--- hosts/foundry/default.nix
+++ hosts/foundry/default.nix
@@ -15,6 +15,16 @@
     efi.canTouchEfiVariables = true;
     efi.efiSysMountPoint = "/boot";
   };
+  hardware.firmware = [
+    (pkgs.stdenvNoCC.mkDerivation (final: {
+      name = "brcm-firmware";
+      src = ./firmware/brcm;
+      installPhase = ''
+        mkdir -p $out/lib/firmware/brcm
+        cp ${final.src}/* "$out/lib/firmware/brcm"
+      '';
+    }))
+  ];
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   networking.hostName = "foundry";
 
